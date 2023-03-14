719678e114234a250152629c888eabe67507bab5
Sandell390 <sandell327@gmail.com>
2023-03-14 23:35:48 +0100

    Done

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ECarsData } from 'src/app/interfaces/ecars-data';
 import { CardataService } from 'src/app/services/cardata.service';
@@ -12,7 +12,7 @@ export class CarComponent {
 
   ObserverCar: Observable<ECarsData[]> | undefined;
 
-  constructor(private cardataService: CardataService) {
+  constructor(private renderer: Renderer2, private el: ElementRef, private cardataService: CardataService) {
     console.log('constructor');
     //const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
     this.ObserverCar = this.cardataService.CarOberserver;
